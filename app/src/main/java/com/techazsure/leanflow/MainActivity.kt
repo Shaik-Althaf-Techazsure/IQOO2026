99e90be8c05c226fcf389ad2926cb15789053a2f
gytdrop <shaikaktharali2022@gmail.com>
2026-06-14 00:41:54 +0530

    the learnscreen and teh mainactivity are updates commited again

--- app/src/main/java/com/techazsure/leanflow/MainActivity.kt
+++ app/src/main/java/com/techazsure/leanflow/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LearnflowlyScreen(cameraEngine)
+                    LearnflowlyScreen(cameraEngine, aiEngine)
                 }
             }
         }
